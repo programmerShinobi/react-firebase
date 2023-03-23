f46b70d84b85a0b15bce8aa9ded0e18814011a35
programmerShinobi <faqihpratamamuhti@gmail.com>
2023-03-23 12:04:53 +0700

    update halaman registrasi

--- src/pages/registrasi/index.js
+++ src/pages/registrasi/index.js
@@ -3,14 +3,69 @@ import * as React from 'react';
 // import komponen material UI
 import Button from '@mui/material/Button';
 import useStyles from './styles';
+import { Container, Grid, Paper, TextField, Typography } from '@mui/material';
+
+// import react router dom
+import { Link } from 'react-router-dom'
 
 function Registrasi() {
-    const classes = useStyles;
+    const styles = useStyles.props.children;
     return (
-        <>
-            <h1 style={{ color: classes.blue }}>Halaman Registrasi</h1>
-            <Button color="primary" variant="contained">Click</Button>
-        </>
+        <Container maxWidth="xs">
+            <Paper style={styles.paper} >
+                <Typography
+                    variant="h5"
+                    component="h1"
+                    style={styles.title}>
+                    Buat Akun Baru
+                </Typography>
+
+                <form>
+                    <TextField
+                        id="email"
+                        type="email"
+                        name="email"
+                        label="Alamat Email"
+                        margin="normal"
+                        variant="standard"
+                        fullWidth
+                        required
+                    />
+                    <TextField
+                        id="password"
+                        type="password"
+                        name="password"
+                        label="Password"
+                        margin="normal"
+                        variant="standard"
+                        fullWidth
+                        required
+                    />
+                    <TextField
+                        id="ulangi_password"
+                        type="password"
+                        name="ulangi_password"
+                        label="Ulangi Password"
+                        margin="normal"
+                        variant="standard"
+                        fullWidth
+                        required
+                    />
+                    <Grid container style={styles.buttons} >
+                        <Grid item xs>
+                            <Button size="large" type="reset" color="error" variant="contained">Cancle</Button>
+                        </Grid>
+                        <Grid item>
+                            <Button size="large" type="submit" color="primary" variant="contained">Daftar</Button>
+                        </Grid>
+                    </Grid>
+                    <Typography style={styles.label}>
+                        Anda punya akun lain?&nbsp;
+                        <Link to="/login" style={styles.labelLink}>Login</Link>
+                    </Typography>
+                </form>
+            </Paper>
+        </Container >
     );
 }
 
--- src/pages/registrasi/styles.js
+++ src/pages/registrasi/styles.js
@@ -1,8 +1,42 @@
 import theme from "../../config/theme";
+import { StyledEngineProvider } from "@mui/material";
 
-const useStyles = {
-    blue: theme.palette.primary[500]
-}
+const useStyles = (
+    <StyledEngineProvider>
+        {
+            {
+                title: {
+                    // color: theme.palette.primary.main
+                    textAlign: 'center',
+                    marginBottom: theme.spacing(3),
+                },
+
+                paper: {
+                    marginTop: theme.spacing(8),
+                    padding: theme.spacing(6)
+                },
+
+                buttons: {
+                    marginTop: theme.spacing(6)
+                },
+
+                label: {
+                    marginTop: theme.spacing(6),
+                    textAlign: 'center',
+                    color: 'gray'
+                },
+
+                labelLink: {
+                    color: theme.palette.primary.main
+                }
+
+            }
+        }
+    </StyledEngineProvider>
+)
+
+
+
 
 export default useStyles;
 
