15441bd0aa597bb7e575c4e2d86ad63e485c3406
programmerShinobi <faqihpratamamuhti@gmail.com>
2023-03-30 14:24:11 +0700

    updata home page

--- src/pages/private/home/index.js
+++ src/pages/private/home/index.js
@@ -18,6 +18,7 @@ function Home() {
                         })
                 }
                 color="error"
+                variant="contained"
             >
                 Sign Out
             </Button>
