4364ea5c97345d2bf5533157e6c5d237f67e777e
programmerShinobi <faqihpratamamuhti@gmail.com>
2023-03-30 11:24:26 +0700

    update lupa password page

--- src/pages/lupa-password/index.js
+++ src/pages/lupa-password/index.js
@@ -1,6 +1,16 @@
 import React from "react";
+import { useFirebase } from "../../components/FirebaseProvider";
+import { Redirect } from "react-router-dom";
+import AppLoading from "../../components/AppLoading";
 
 function LupaPassword() {
+    const firebase = useFirebase();
+    if (firebase.loading) {
+        return (<AppLoading />)
+    }
+    if (firebase.user) {
+        return (<Redirect to='/' />)
+    }
     return (<h1>Halaman Lupa Password</h1>);
 }
 
---
+++ src/pages/lupa-password/styles.js
@@ -0,0 +1,39 @@
+import theme from "../../config/theme";
+import { StyledEngineProvider } from "@mui/material";
+
+const useStyles = (
+    <StyledEngineProvider>
+        {
+            {
+                title: {
+                    // color: theme.palette.primary.main
+                    textAlign: 'center',
+                    marginBottom: theme.spacing(3),
+                },
+
+                paper: {
+                    marginTop: theme.spacing(8),
+                    padding: theme.spacing(6)
+                },
+
+                buttons: {
+                    marginTop: theme.spacing(6)
+                },
+
+                label: {
+                    marginTop: theme.spacing(6),
+                    textAlign: 'center',
+                    color: 'gray'
+                },
+
+                labelLink: {
+                    color: theme.palette.primary.main
+                }
+
+            }
+        }
+    </StyledEngineProvider>
+);
+
+export default useStyles;
+
