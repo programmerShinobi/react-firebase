a2e3e822e7925c5863a94eaddc44fa153b37c733
programmerShinobi <faqihpratamamuhti@gmail.com>
2023-04-02 15:21:28 +0700

    update private pages

--- src/pages/private/styles.js
+++ src/pages/private/styles.js
@@ -41,7 +41,7 @@ export const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 
                 }),
                 width: theme.spacing(7),
                 [theme.breakpoints.up('sm')]: {
-                    width: theme.spacing(9),
+                    width: theme.spacing(7),
                 },
             }),
         },
