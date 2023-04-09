2f7fa73f1a17a250513f3969ab4f0fb39c919e60
programmerShinobi <faqihpratamamuhti@gmail.com>
2023-04-09 16:03:09 +0700

    add utils : formatter (Rupiah)

---
+++ src/utils/formatter.js
@@ -0,0 +1,24 @@
+import numeral from "numeral";
+
+// rupiah / indonesia (id)
+numeral.register('locale', 'id', {
+    delimiters: {
+        thousands: '.',
+        decimal: ',',
+    },
+    abbreviations: {
+        thousand: 'rb',
+        million: 'jt',
+        billion: 'm',
+        trillion: 't'
+    },
+    currency: {
+        symbol: 'Rp '
+    }
+});
+
+numeral.locale('id');
+
+export const currency = (number) => {
+    return numeral(number).format('$0.0');
+}
