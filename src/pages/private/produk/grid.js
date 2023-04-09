bc5ca95726d138e3ce3ed3bc698723582d4d6b45
programmerShinobi <faqihpratamamuhti@gmail.com>
2023-04-09 16:03:37 +0700

    update private page (produk) : halaman daftar produk

--- src/pages/private/produk/grid.js
+++ src/pages/private/produk/grid.js
@@ -1,20 +1,117 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 // material-ui
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
 import useStyles from "./styles/grid";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions"
+import Typography from "@mui/material/Typography";
+import ImageIcon from "@mui/icons-material/Image";
 
 // page
 import AddDialog from "./add";
+import AppPageLoading from "../../../components/AppPageLoading";
+
+// firebase
+import { useFirebase } from "../../../components/FirebaseProvider";
+import { collection } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore"
+import { currency } from "../../../utils/formatter";
 
 function GridProduk() {
     const [openAddDialog, setOpenAddDialog] = useState(false);
 
+    const firebase = useFirebase();
+
+    const produkCol = collection(firebase.firestore, `toko/${firebase.user.uid}/produk`)
+
+    const [snapshot, loading] = useCollection(produkCol);
+
+    const [produkItems, setProdukItems] = useState([]);
+
+    useEffect(() => {
+        setProdukItems(snapshot?.docs);
+    }, [snapshot]);
+
+    console.info(produkItems);
+
+    if (loading) {
+        return (<AppPageLoading />);
+    }
+
     const styles = useStyles.props.children;
     return (
         <>
-            <h1>Halaman Grid Produk</h1>
+            <Typography
+                variant="h5"
+                component="h1"
+            >
+                Daftar Produk
+            </Typography>
+            {
+                produkItems?.length <= 0 && (
+                    <Typography>Belum ada produk</Typography>
+                )
+            }
+
+            <Grid container spacing={5}>
+                {
+                    produkItems?.map((produkDoc) => {
+                        const produkData = produkDoc.data();
+                        return (
+                            <Grid
+                                key={produkDoc.id}
+                                item={true}
+                                xs={12}
+                                sm={12}
+                                md={6}
+                                lg={4}
+                            >
+                                <Card style={styles.card}>
+                                    {produkData?.foto && (
+                                        <CardMedia
+                                            style={styles.foto}
+                                            image={produkData.foto}
+                                            title={produkData.nama}
+                                        />
+                                    )}
+                                    {!produkData?.foto && (
+                                        <div style={styles.fotoPlaceholder}>
+                                            <ImageIcon
+                                                fontSize="large"
+                                                color="disabled"
+                                            />
+                                        </div>
+                                    )}
+                                    <CardContent>
+                                        <Typography
+                                            variant="h5"
+                                            noWrap
+                                        >
+                                            {produkData.nama}
+                                        </Typography>
+                                        <Typography
+                                            variant="subtitle1"
+                                        >
+                                            Stok : {produkData.stok}
+                                        </Typography>
+                                        <Typography
+                                            variant="subtitle1"
+                                        >
+                                            {currency(produkData.harga)}
+                                        </Typography>
+
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        );
+                    })
+                }
+            </Grid>
             <Fab
                 style={styles.fab}
                 color="primary"
--- src/pages/private/produk/styles/grid.js
+++ src/pages/private/produk/styles/grid.js
@@ -9,6 +9,17 @@ const useStyles = (
                     position: 'absolute',
                     bottom: theme.spacing(2),
                     right: theme.spacing(2)
+                },
+                card: {
+                    display: 'flex'
+                },
+                foto: {
+                    width: 150
+                },
+                fotoPlaceholder: {
+                    width: 150,
+                    alignSelf: 'center',
+                    textAlign: 'center'
                 }
             }
         }
